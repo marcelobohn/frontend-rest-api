3b73f7b3dccf29fe132afeef0cca6322b7d09fa7
Marcelo Bohn <marcelo.bohn@digitaldesk.com.br>
2016-05-17 14:07:58 -0300

    basic

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,26 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var htmlmin = require('gulp-htmlmin');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('build')
+    .pipe(webserver({
+      fallback: 'build/index.html'
+    }));
+});
+
+gulp.task('jshint', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('minify', function() {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'))
+});
 
 gulp.task('default', function(){ 
     console.log('it´s running!'); 
